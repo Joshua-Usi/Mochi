fdfbffadfa89c96ac182dd4ce3e6dacac41e847d
Joshua usi <joshua.usi2017@gmail.com>
2023-02-28 17:20:38 +1100

    Updated Mochi to use references

--- Mochi.h
+++ Mochi.h
@@ -117,7 +117,7 @@ namespace Mochi
 			type state;
 			int chainSize;
 		public:
-			That* Is = this;
+			That& Is = *this;
 			That(type value)
 			{
 				state = value;
@@ -130,114 +130,114 @@ namespace Mochi
 			/// <summary>
 			/// Logical NOT gate that inverts the next operation to return the opposite boolean value
 			/// </summary>
-			That* Not()
+			That& Not()
 			{
 				Logic::Gate* inverter = new Logic::Gate(Logic::Operator::Not, 1);
 				inverter->SetInput(chainSize, satisfaction);
 				satisfaction = inverter;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Logical OR gate that returns true if one or more inputs is true
 			/// </summary>
-			That* Or()
+			That& Or()
 			{
 				satisfaction->SetOperator(Logic::Operator::Or);
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Logical AND gate that only returns true if all inputs are true
 			/// </summary>
 			/// <param name="value">The values to compare with</param>
-			That* And()
+			That& And()
 			{
 				satisfaction->SetOperator(Logic::Operator::And);
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Comparator that returns true if the given value is equal to the asserted value
 			/// </summary>
 			/// <param name="value">Value to compare</param>
-			That* EqualTo(type value)
+			That& EqualTo(type value)
 			{
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, state == value);
 				chainSize++;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Comparator that returns true if the given value is less than the asserted value
 			/// </summary>
 			/// <param name="value">Value to compare</param>
-			That* LessThan(type value)
+			That& LessThan(type value)
 			{
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, state < value);
 				chainSize++;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Comparator that returns true if the given value is greater than the asserted value
 			/// </summary>
 			/// <param name="value">Value to compare</param>
-			That* GreaterThan(type value)
+			That& GreaterThan(type value)
 			{
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, state > value);
 				chainSize++;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Floating point comparator that returns true if the asserted value is NaN
 			/// </summary>
-			That* NaN()
+			That& NaN()
 			{
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, std::isnan(state));
 				chainSize++;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Floating point comparator that returns true if the asserted value is Finite
 			/// </summary>
-			That* Finite()
+			That& Finite()
 			{
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, !std::isinf(state));
 				chainSize++;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Boolean comparator that returns true if the asserted value is true or truthy
 			/// </summary>
-			That* True()
+			That& True()
 			{
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, state);
 				chainSize++;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Memory comparator that returns true if the 2 memory locations are exactly equal bit for bit
 			/// </summary>
 			/// <param name="memregion">Starting address of memory region to compare</param>
-			That* Equals(type memregion)
+			That& Equals(type memregion)
 			{
 				int size = sizeof(type);
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, memcmp(state, memregion, size) == 0);
 				chainSize++;
-				return this;
+				return *this;
 			}
 			/// <summary>
 			/// Memory comparator that returns true if the memory address points to a Null Address (0x0)
 			/// </summary>
-			That* Null()
+			That& Null()
 			{
 				satisfaction->SetInputCount(chainSize + 1);
 				satisfaction->SetInputBool(chainSize, state == 0);
 				chainSize++;
-				return this;
+				return *this;
 			}
 			bool AssertsTrue()
 			{
